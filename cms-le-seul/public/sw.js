ef4cc22bcfda4d6de1a297c1d0d1ec16f31a2f26
yawo228 <edohbernard1@gmail.com>
2026-03-06 22:39:07 +0000

    Delete cms-le-seul directory

--- cms-le-seul/public/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('SW installé');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Nécessaire pour valider les critères PWA de Chrome
-});
